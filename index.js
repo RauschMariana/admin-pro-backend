af6a5a59e55c443c2e8658b010704dec52aa7605
Mariana <marianarauschb@gmail.com>
2024-10-08 13:13:27 -0300

    Cors y Express - first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de express
+const app = express();
+
+// Configurar CORS
+app.use(cors());
+
+// Base de datos
+dbConnection();
+
+// Rutas
+app.get('/', (req, res) => {
+    res.json({ 
+        ok: true,
+        msg: 'Hola, soy una API' 
+    });
+});
+
+
+app.listen( process.env.PORT, () => {
+    console.log('Servidor corriendo en el puerto ' + process.env.PORT);
+});
