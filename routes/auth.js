46caa1f8f8815aad00547c65c46646e4e3874ddc
Mariana <marianarauschb@gmail.com>
2024-10-13 23:57:19 -0300

    Google Sing-in

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@
  */
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validateFields } = require('../middlewares/validate-field');
 
 
@@ -18,6 +18,15 @@ router.post(
         validateFields
     ], 
     login
+);
+
+router.post(
+    '/google', 
+    [
+        check('token', 'El token de Google es obligatorio').not().isEmpty(),
+        validateFields
+    ], 
+    googleSignIn
 );
 
 
